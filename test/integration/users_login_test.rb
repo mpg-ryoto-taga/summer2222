2747584187cb576a2c755f03f6d8f84047f6132b
mpg-teruhisa-fukumoto <teruhisa.fukumoto@medpeer.co.jp>
2022-08-22 16:43:07 +0900

    8.2 ログイン を完了
    "8.2 ログイン"の章を完了した。
    具体的には、以下の追加・実装を行った。
    
    1. `SessionHelper`に`log_in`メソッドを追加し、
       ログインが必要な処理で実行する
    2. `current_user`メソッドの追加
    3. ヘッダーの修正
    4. 以下のコマンドを実行して、jQueryとBootstrapを追加
    
    ```
    $ docker-compose run web yarn add jquery@3.4.1 bootstrap@3.4.1
    ```
    
    ※URL: https://railstutorial.jp/chapters/basic_login?version=6.0#sec-logging_in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  def after_setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: "michael@example.com",
+                                          password: "invalid" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
